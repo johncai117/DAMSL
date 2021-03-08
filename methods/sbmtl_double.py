385a2b0f6bd45202bdfd11738dc0a5e6688898db
johncai117 <johncai117@gmail.com>
2021-03-08 21:05:45 +0800

    add train_double

---
+++ methods/sbmtl_double.py
@@ -0,0 +1,355 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from methods.meta_template import MetaTemplate
+from methods.gnn import GNN_nl
+
+from torch.autograd import Variable
+import backbone
+import copy
+import math
+import random
+import torch.nn.functional as F
+import configs
+from utils import device
+from methods.baselinetrain import BaselineTrain
+from io_utils import model_dict, parse_args, get_resume_file, get_assigned_file
+
+
+
+class Classifier(nn.Module):
+    def __init__(self, dim, n_way):
+        super(Classifier, self).__init__()
+        
+        self.fc = nn.Linear(dim, n_way)
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+
+class GnnNet(MetaTemplate):
+  maml=False
+  def __init__(self, model_func,  n_way, n_support):
+    super(GnnNet, self).__init__(model_func, n_way, n_support)
+
+    # loss function
+    self.loss_fn = nn.CrossEntropyLoss()
+    self.first = True
+
+    # metric function
+    self.fc = nn.Sequential(nn.Linear(self.feat_dim, 64), nn.BatchNorm1d(64, track_running_stats=False)) 
+    self.fc2 = nn.Sequential(nn.Linear(n_way, 32), nn.BatchNorm1d(32, track_running_stats=False)) 
+    self.fc3 = nn.Sequential(nn.Linear(n_way, 32), nn.BatchNorm1d(32, track_running_stats=False)) 
+    self.gnn = GNN_nl(64 + self.n_way, 32, self.n_way)
+    self.method = 'GnnNet'
+
+    ## batchnorm and classifier
+    self.classifier = Classifier(self.feat_dim, self.n_way)
+    self.batchnorm = nn.BatchNorm1d(5, track_running_stats=False)
+        
+    
+    
+    # number of layers to allow to adapt during fine-tuning
+    self.num_FT_block = 2 ##default
+    self.ft_epoch = 3
+
+    self.num_FT_layers = -9
+
+    # fix label for training the metric function   1*nw(1 + ns)*nw
+    support_label = torch.from_numpy(np.repeat(range(self.n_way), self.n_support)).unsqueeze(1)
+    support_label = torch.zeros(self.n_way*self.n_support, self.n_way).scatter(1, support_label, 1).view(self.n_way, self.n_support, self.n_way)
+    support_label = torch.cat([support_label, torch.zeros(self.n_way, 1, n_way)], dim=1)
+    self.support_label = support_label.view(1, -1, self.n_way)
+
+  def cuda(self):
+    self.feature_baseline2.to(device)
+    self.fc.to(device)
+    self.fc2.to(device)
+    self.fc3.to(device)
+    self.gnn.to(device)
+    self.batchnorm.to(device)
+    self.classifier.to(device)
+    
+    self.support_label = self.support_label.to(device)
+    return self
+
+  def instantiate_baseline(self, params):
+    def load_baseline(num):
+      baseline_model  = BaselineTrain( backbone.ResNet10, 64)
+      save_dir =  configs.save_dir
+      self.params = copy.deepcopy(params)
+      self.params.checkpoint_dir = '%s/checkpoints/%s/%s_%s' %(save_dir, "miniImageNet", "ResNet10", "baseline")
+      if self.params.train_aug:
+          self.params.checkpoint_dir += '_aug'
+      
+      resume_file2 = get_assigned_file(self.params.checkpoint_dir, 400)
+      if resume_file2 is not None:
+        tmp = torch.load(resume_file2)
+        
+        state = tmp['state']
+        state_keys = list(state.keys())
+        for _, key in enumerate(state_keys):
+            if "feature2." in key:
+                state.pop(key)
+            if "feature3." in key:
+                state.pop(key)
+            elif "feature" in key:
+                newkey = key.replace("feature.", "")
+                state[newkey] = state.pop(key)
+            else:
+                state.pop(key)
+            
+              
+      baseline_model.feature.load_state_dict(state)  
+      return baseline_model.feature
+    self.feature_baseline = copy.deepcopy(load_baseline(400))
+    self.feature_baseline2 = copy.deepcopy(load_baseline(450))
+    self.batchnorm2 = nn.BatchNorm1d(5, track_running_stats=False)
+    self.fc_new = nn.Sequential(nn.Linear(10, 64), nn.BatchNorm1d(64, track_running_stats=False)) 
+    self.batchnorm2.to(device)
+    self.feature_baseline.to(device)
+    self.feature_baseline2.to(device)
+    self.fc_new.to(device)
+
+  
+
+  def set_forward(self,x,is_feature=False):
+    x = x.to(device)
+
+    if is_feature:
+      # reshape the feature tensor: n_way * n_s + 15 * f
+      assert(x.size(1) == self.n_support + 15)
+      z = self.fc(x.view(-1, *x.size()[2:]))
+      z = z.view(self.n_way, -1, z.size(1))
+    else:
+      # get feature using encoder
+      x = x.view(-1, *x.size()[2:])
+      z = self.fc(self.feature_baseline2(x))
+      z = z.view(self.n_way, -1, z.size(1))
+
+    # stack the feature for metric function: n_way * n_s + n_q * f -> n_q * [1 * n_way(n_s + 1) * f]
+    z_stack = [torch.cat([z[:, :self.n_support], z[:, self.n_support + i:self.n_support + i + 1]], dim=1).view(1, -1, z.size(2)) for i in range(self.n_query)]
+    
+    assert(z_stack[0].size(1) == self.n_way*(self.n_support + 1))
+    scores = self.forward_gnn(z_stack)
+    return scores
+
+  def train_loop_finetune(self, epoch, train_loader, optimizer ):
+        ### load baseline model
+
+        print_freq = 10
+        avg_loss=0
+        for i, (x,_ ) in enumerate(train_loader):
+            self.n_query = x.size(1) - self.n_support           
+            if self.change_way:
+                self.n_way  = x.size(0)
+            optimizer.zero_grad()
+            loss = self.set_forward_loss_finetune( x )
+            loss.backward()
+            optimizer.step()
+            avg_loss = avg_loss+loss.item()
+
+            if i % print_freq==0:
+                #print(optimizer.state_dict()['param_groups'][0]['lr'])
+                print('Epoch {:d} | Batch {:d}/{:d} | Loss {:f}'.format(epoch, i, len(train_loader), avg_loss/float(i+1)))
+  
+  def MAML_update(self):
+    names = []
+    for name, param in self.feature.named_parameters():
+      if param.requires_grad:
+        #print(name)
+        names.append(name)
+    
+    names_sub = names[:self.num_FT_layers] ### last Resnet block can adapt
+    if not self.first:
+      for (name, param), (name1, param1), (name2, param2) in zip(self.feature.named_parameters(), self.feature2.named_parameters(), self.feature3.named_parameters()):
+        if name not in names_sub:
+          dat_change = param2.data - param1.data ### Y - X
+          new_dat = param.data - dat_change ### (Y- V) - (Y-X) = X-V
+          param.data.copy_(new_dat)
+      for (name, param), (name1, param1), (name2, param2) in zip(self.classifier.named_parameters(), self.classifier2.named_parameters(), self.classifier3.named_parameters()):
+        if name not in names_sub:
+          dat_change = param2.data - param1.data ### Y - X
+          new_dat = param.data - dat_change ### (Y- V) - (Y-X) = X-V
+          param.data.copy_(new_dat)
+
+  def set_forward_finetune(self,x,is_feature=False, linear = False):
+    x = x.to(device)
+    # get feature using encoder
+    support_size = self.n_way * self.n_support 
+    batch_size = 8
+
+    for name, param  in self.feature_baseline2.named_parameters():
+      param.requires_grad = True
+
+    x_var = Variable(x)
+      
+    y_a_i = Variable( torch.from_numpy( np.repeat(range( self.n_way ), self.n_support ) )).to(device) # (25,)
+    
+    x_b_i = x_var[:, self.n_support:,:,:,:].contiguous().view( self.n_way* self.n_query,   *x.size()[2:]) 
+    x_a_i = x_var[:,:self.n_support,:,:,:].contiguous().view( self.n_way* self.n_support, *x.size()[2:])
+    x_inn = x_var.view(self.n_way* (self.n_support + self.n_query), *x.size()[2:]).to(device)
+    ### copy over feature and classifier of the main model
+    feat_network = copy.deepcopy(self.feature_baseline2)
+    classifier = copy.deepcopy(self.classifier)
+
+
+    #### copy baseline feature and instantiate classifer
+    baseline_feat = copy.deepcopy(self.feature_baseline)
+    classifier_baseline = Classifier(self.feature_baseline.final_feat_dim, self.n_way) ##instantiate classifier
+    classifier_baseline.to(device)
+
+    ### select layers to freeze
+    names = []
+    for name, param in feat_network.named_parameters():
+      if param.requires_grad:
+        #print(name)
+        names.append(name)
+    
+    names_sub = names[:-9] ### last Resnet block can adapt
+
+    for name, param in feat_network.named_parameters():
+      if name in names_sub:
+        param.requires_grad = False   
+
+    ### loss function and oiptimizer
+    delta_opt = torch.optim.Adam(filter(lambda p: p.requires_grad, feat_network.parameters()), lr = 0.01)
+    classifier_opt = torch.optim.Adam(classifier.parameters(), lr = 0.01)
+
+    ### freeze layers of baseline feat
+    names_b = []
+
+    for _, param in baseline_feat.named_parameters():
+            param.requires_grad = True
+
+    for name, param in baseline_feat.named_parameters():
+      if param.requires_grad:
+        #print(name)
+        names.append(name)
+    
+    names_sub_b = names_b[:-9] ### last Resnet block can adapt
+
+    for name, param in baseline_feat.named_parameters():
+      if name in names_sub_b:
+        param.requires_grad = False   
+
+    delta_opt_b = torch.optim.Adam(filter(lambda p: p.requires_grad, baseline_feat.parameters()), lr = 0.01)
+    classifier_opt_b = torch.optim.Adam(classifier_baseline.parameters(), lr = 0.01)
+
+
+    loss_fn = nn.CrossEntropyLoss().to(device) 
+    if self.params.n_shot <= 20:
+      total_epoch = 15
+    else:
+      total_epoch = 5
+
+    classifier.train()
+    feat_network.train()
+
+    classifier.to(device)
+    feat_network.to(device)
+
+    for epoch in range(total_epoch):
+          rand_id = np.random.permutation(support_size)
+
+          for j in range(0, support_size, batch_size):
+              classifier_opt.zero_grad()
+              
+              delta_opt.zero_grad()
+
+              #####################################
+              selected_id = torch.from_numpy( rand_id[j: min(j+batch_size, support_size)]).to(device)
+              
+              z_batch = x_a_i[selected_id]
+              y_batch = y_a_i[selected_id] 
+              #####################################
+
+              output = feat_network(z_batch)
+              score  = classifier(output)
+              loss = loss_fn(score, y_batch)
+              #grad = torch.autograd.grad(set_loss, fast_parameters, create_graph=True)
+
+              #####################################
+              loss.backward() ### think about how to compute gradients and achieve a good initialization
+
+              classifier_opt.step()
+              delta_opt.step()
+
+
+    for epoch in range(total_epoch):
+          rand_id = np.random.permutation(support_size)
+
+          for j in range(0, support_size, batch_size):
+              classifier_opt_b.zero_grad()
+              
+              delta_opt_b.zero_grad()
+
+              #####################################
+              selected_id = torch.from_numpy( rand_id[j: min(j+batch_size, support_size)]).to(device)
+              
+              z_batch = x_a_i[selected_id]
+              y_batch = y_a_i[selected_id] 
+              #####################################
+
+              output = baseline_feat(z_batch)
+              score  = classifier_baseline(output)
+              loss_b = loss_fn(score, y_batch)
+              #grad = torch.autograd.grad(set_loss, fast_parameters, create_graph=True)
+
+              #####################################
+              loss_b.backward() ### think about how to compute gradients and achieve a good initialization
+
+              classifier_opt_b.step()
+              delta_opt_b.step()
+    
+    
+
+    output_all = feat_network(x_inn.to(device)).view(self.n_way, self.n_support + self.n_query, -1).detach()
+    final = classifier(output_all)
+    final = torch.transpose(self.batchnorm(torch.transpose(final, 1,2)),1,2).contiguous()
+
+    ### load baseline feature
+    output_all_b = baseline_feat(x_inn.to(device)).view(self.n_way, self.n_support + self.n_query, -1).detach()
+    final_b = classifier_baseline(output_all_b).detach()
+    final_b = torch.transpose(self.batchnorm2(torch.transpose(final_b, 1,2)),1,2).contiguous()
+    
+    ### feed into fc and gnn
+
+    assert(final.size(1) == self.n_support + 16) ##16 query samples in each batch
+
+    z = self.fc2(final.view(-1, *final.size()[2:]))
+    z = z.view(self.n_way, -1, z.size(1))
+
+    z_b = self.fc2(final_b.view(-1, *final_b.size()[2:]))
+    z_b = z_b.view(self.n_way, -1, z_b.size(1))
+
+    z = torch.cat([z, z_b], dim = 2)
+
+    z_stack = [torch.cat([z[:, :self.n_support], z[:, self.n_support + i:self.n_support + i + 1]], dim=1).view(1, -1, z.size(2)) for i in range(self.n_query)]
+    
+    assert(z_stack[0].size(1) == self.n_way*(self.n_support + 1))
+    
+    scores = self.forward_gnn(z_stack)
+    return scores
+
+  def forward_gnn(self, zs):
+    # gnn inp: n_q * n_way(n_s + 1) * f
+    nodes = torch.cat([torch.cat([z, self.support_label.to(device)], dim=2) for z in zs], dim=0)
+    scores = self.gnn(nodes)
+    # n_q * n_way(n_s + 1) * n_way -> (n_way * n_q) * n_way
+    scores = scores.view(self.n_query, self.n_way, self.n_support + 1, self.n_way)[:, :, -1].permute(1, 0, 2).contiguous().view(-1, self.n_way)
+    return scores
+
+  def set_forward_loss(self, x):
+    y_query = torch.from_numpy(np.repeat(range( self.n_way ), self.n_query))
+    y_query = y_query.to(device)
+    scores = self.set_forward(x)
+    loss = self.loss_fn(scores, y_query)
+    return loss
+
+  def set_forward_loss_finetune(self, x):
+    y_query = torch.from_numpy(np.repeat(range( self.n_way ), self.n_query))
+    y_query = y_query.to(device)
+    scores = self.set_forward_finetune(x)
+    loss = self.loss_fn(scores, y_query)
+    return loss
---
+++ train_double.py
@@ -0,0 +1,209 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim
+import torch.optim.lr_scheduler as lr_scheduler
+import time
+import os
+import glob
+from methods import sbmtl_double as sbmtl
+from methods import gnnnet
+from methods import gnn
+
+import configs
+import backbone
+from data.datamgr import SimpleDataManager, SetDataManager
+from methods.baselinetrain import BaselineTrain
+from methods.protonet import ProtoNet
+from io_utils import model_dict, parse_args, get_resume_file, get_assigned_file
+from datasets import miniImageNet_few_shot, DTD_few_shot
+from utils import device
+configs.save_dir = 'logs_final_train' ##override
+
+def train(base_loader, model, optimization, start_epoch, stop_epoch, params):  
+    for _, param in model.named_parameters():
+            param.requires_grad = True
+    
+    if params.method == "sbmtl" and params.start_epoch >= 401:
+        for _, param in model.feature_baseline.named_parameters():
+            param.requires_grad = False  
+
+    if optimization == 'SGD':
+        optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr = 0.01, momentum = 0.9, weight_decay = 0.00005 )
+    elif optimization == "Adam":
+        optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()))
+    else:
+       raise ValueError('Unknown optimization, please define by yourself')     
+    model.train()
+
+    if not params.fine_tune:
+      for epoch in range(start_epoch,stop_epoch):
+          if params.method == "gnnnet":
+            model.train_loop2(epoch, base_loader,  optimizer ) 
+          else:
+            model.train_loop(epoch, base_loader,  optimizer )
+          if not os.path.isdir(params.checkpoint_dir):
+              os.makedirs(params.checkpoint_dir)
+
+          if (epoch % params.save_freq==0) or (epoch==stop_epoch-1):
+              outfile = os.path.join(params.checkpoint_dir, '{:d}.tar'.format(epoch))
+              torch.save({'epoch':epoch, 'state':model.state_dict()}, outfile)
+    else:
+      for epoch in range(start_epoch,stop_epoch):
+          model.train()
+          model.train_loop_finetune(epoch, base_loader,  optimizer ) 
+
+          if (epoch % params.save_freq==0) or (epoch==stop_epoch-1):
+              outfile = os.path.join(params.checkpoint_dir, '{:d}.tar'.format(epoch))
+              torch.save({'epoch':epoch, 'state':model.state_dict()}, outfile)
+         
+            
+     
+    return model
+
+if __name__=='__main__':
+    print("HELLO")
+    
+    params = parse_args('train')
+    print(params.method)
+    if not params.start_epoch > 0:
+      np.random.seed(10) #original was 10
+
+    image_size = 224
+    optimization = 'Adam'
+
+    if params.method in ['baseline'] :
+
+        if params.dataset == "miniImageNet":
+            #print('hi')
+            datamgr = miniImageNet_few_shot.SimpleDataManager(image_size, batch_size = 16)
+            #print("bye")
+            base_loader = datamgr.get_data_loader(aug = params.train_aug )
+            params.num_classes = 64
+            #print("loaded")
+        elif params.dataset == "CUB":
+
+            base_file = configs.data_dir['CUB'] + 'base.json' 
+            base_datamgr    = SimpleDataManager(image_size, batch_size = 16)
+            base_loader     = base_datamgr.get_data_loader( base_file , aug = params.train_aug )
+       
+        elif params.dataset == "cifar100":
+            base_datamgr    = cifar_few_shot.SimpleDataManager("CIFAR100", image_size, batch_size = 16)
+            base_loader    = base_datamgr.get_data_loader( "base" , aug = True )
+                
+            params.num_classes = 100
+
+        elif params.dataset == 'caltech256':
+            base_datamgr  = caltech256_few_shot.SimpleDataManager(image_size, batch_size = 16)
+            base_loader = base_datamgr.get_data_loader(aug = False )
+            params.num_classes = 257
+
+        elif params.dataset == "DTD":
+            base_datamgr    = DTD_few_shot.SimpleDataManager(image_size, batch_size = 16)
+            base_loader     = base_datamgr.get_data_loader( aug = True )
+
+        else:
+           raise ValueError('Unknown dataset')
+        
+        #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        #print(device)
+        model           = BaselineTrain( model_dict[params.model], params.num_classes)
+
+    elif params.method in ['sbmtl','maml','relationnet','protonet', 'gnnnet', 'metaoptnet']:
+        n_query = max(1, int(16* params.test_n_way/params.train_n_way)) #if test_n_way is smaller than train_n_way, reduce n_query to keep batch size small
+        train_few_shot_params    = dict(n_way = params.train_n_way, n_support = params.n_shot) 
+        test_few_shot_params     = dict(n_way = params.test_n_way, n_support = params.n_shot) 
+
+        if params.dataset == "miniImageNet":
+            print("loading")
+            datamgr            = miniImageNet_few_shot.SetDataManager(image_size, n_query = n_query,  **train_few_shot_params)
+            base_loader        = datamgr.get_data_loader(aug = params.train_aug)
+            #datamgr         = miniImageNet_few_shot.SimpleDataManager(image_size, batch_size = 64)
+            #data_loader     = datamgr.get_data_loader(aug = False )
+            
+            print("BYE")
+
+        else:
+           raise ValueError('Unknown dataset')
+
+        if params.method == 'protonet':
+            model           = ProtoNet( model_dict[params.model], **train_few_shot_params )
+        elif params.method == 'protonet_damp':
+            model           = protonet_damp.ProtoNet( model_dict[params.model], **train_few_shot_params )
+        elif params.method == 'relationnet':
+            feature_model = lambda: model_dict[params.model]( flatten = False )
+            loss_type = 'mse' if params.method == 'relationnet' else 'softmax'
+            model           = RelationNet(feature_model, loss_type = loss_type , **train_few_shot_params )
+        elif params.method == 'maml':
+            backbone.SimpleBlock.maml = True
+            backbone.BottleneckBlock.maml = True
+            backbone.ResNet.maml = True
+            model           = MAML( model_dict[params.model], **train_few_shot_params )
+        elif params.method == 'metaoptnet':
+            model           = MetaOptNet( model_dict[params.model], **train_few_shot_params )
+        elif params.method == 'gnnnet':
+            model           = GnnNet( model_dict[params.model], **train_few_shot_params)
+        elif params.method == 'sbmtl':
+            model           = sbmtl.GnnNet( model_dict[params.model], **train_few_shot_params)
+        
+       
+       
+    else:
+       raise ValueError('Unknown method')
+
+    model = model.cuda()
+    #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    #print(device)
+    save_dir =  configs.save_dir
+    print("WORKING")
+
+
+    params.checkpoint_dir = '%s/checkpoints/%s/%s_%s' %(save_dir, params.dataset, params.model, params.method)
+    if params.train_aug:
+        params.checkpoint_dir += '_aug'
+
+    if not params.method  in ['baseline', 'baseline++']: 
+        params.checkpoint_dir += '_%dway_%dshot' %( params.train_n_way, params.n_shot)
+
+    if not os.path.isdir(params.checkpoint_dir):
+        os.makedirs(params.checkpoint_dir)
+    
+
+    start_epoch = params.start_epoch
+    stop_epoch = params.stop_epoch
+
+    if params.method == "sbmtl" and params.start_epoch == 400:
+        params.checkpoint_dir = "logs_original_original/logs/checkpoints/miniImageNet/ResNet10_gnnnet_aug_5way_5shot/400.tar"
+
+    print(params.checkpoint_dir)
+  
+    if params.start_epoch > 0:
+    
+      resume_file = get_assigned_file(params.checkpoint_dir, params.start_epoch -1)
+      if resume_file is not None:
+          tmp = torch.load(resume_file)
+          
+          state = tmp['state']
+          state_keys = list(state.keys())
+          for _, key in enumerate(state_keys):
+              if "feature2." in key:
+                  state.pop(key)
+              if "feature3." in key:
+                  state.pop(key)
+          
+          
+      
+      if params.start_epoch == 401 and params.method == "sbmtl":
+          model.load_state_dict(state)
+          model.instantiate_baseline(params)
+      elif params.start_epoch > 401 and params.method == "sbmtl":
+        
+          model.instantiate_baseline(params)
+          model.load_state_dict(state)
+      else:
+          model.load_state_dict(state)
+      model.cuda()
+
+
+    model = train(base_loader, model, optimization, start_epoch, stop_epoch, params)
